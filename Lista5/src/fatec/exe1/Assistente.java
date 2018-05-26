2156c03f1c4086c24b26992c90d150824fc009c4
dfpires <dfpires@gmail.com>
2018-05-25 22:17:55 -0300

    Add files via upload

---
+++ Lista5/src/fatec/exe1/Assistente.java
@@ -0,0 +1,19 @@
+
+package fatec.exe1;
+
+public class Assistente extends Funcionario{
+
+    public Assistente() {
+        super();
+    }
+
+    public Assistente(String nome, String matricula, float salarioBase) {
+        super(nome, matricula, salarioBase);
+    }
+    
+    @Override
+    public float calculaSalario() {
+        return this.salarioBase;
+    }
+    
+}
---
+++ Lista5/src/fatec/exe1/Gerente.java
@@ -0,0 +1,13 @@
+package fatec.exe1;
+public class Gerente extends Funcionario{
+    public Gerente() {
+        super();
+    }
+    public Gerente(String nome, String matricula, float salarioBase) {
+        super(nome, matricula, salarioBase);
+    }
+    @Override
+    public float calculaSalario() {
+        return 2 * this.salarioBase;
+    }
+}
